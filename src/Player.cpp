f678be436f8684af6e1d523ec94bb46d5c0f6a21
sticke2020 <sticke2020@gmail.com>
2026-09-23 15:50:13 -0500

    Created Player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -13,3 +13,63 @@ Player::Player() : m_Speed(START_SPEED),
     // Set the origin of the sprite to the center, for smooth rotation
     m_Sprite.setOrigin(25, 25);
 }
+
+void Player::spawn(IntRect arena, Vector2f resolution, int tileSize) {
+    // Place the player in the middle of the arena
+    m_Position.x = arena.width / 2;
+    m_Position.y = arena.height / 2;
+
+    // Copy the details of the arena to the players m_Arena
+    m_Arena.left = arena.left;
+    m_Arena.width = arena.width;
+    m_Arena.top = arena.top;
+    m_Arena.height = arena.height;
+
+    // Remember how big the tiles are in this arena
+    m_Tilesize = tileSize;
+
+    // Store the resolution for future use
+    m_Resolution.x = resolution.x;
+    m_Resolution.y = resolution.y;
+}
+
+void Player::resetPlayerStats() {
+    m_Speed = START_SPEED;
+    m_Health = START_HEALTH;
+    m_MaxHealth = START_HEALTH;
+}
+
+Time Player::getLastHitTime() {
+    return m_LastHit;
+}
+
+bool Player::hit(Time timeHit) {
+    if (timeHit.asMilliseconds() - m_LastHit.asMilliseconds() > 200) {
+        m_LastHit = timeHit;
+        m_Health -= 10;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+FloatRect Player::getPosition() {
+    return m_Sprite.getGlobalBounds();
+}
+
+Vector2f Player::getCenter() {
+    return m_Position;
+}
+
+float Player::getRotation() {
+    return m_Sprite.getRotation();
+}
+
+Sprite Player::getSprite() {
+    return m_Sprite;
+}
+
+int Player::getHealth() {
+    return m_Health;
+}
