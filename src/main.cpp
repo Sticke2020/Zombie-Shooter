80b24e48fb767931b38b1db17807cda55bffbda4
sticke2020 <sticke2020@gmail.com>
2026-09-24 16:15:26 -0500

    Handle player quitting game and wasd input

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,112 @@
 #include <SFML/Graphics.hpp>
+#include "Player.hpp"
+
+using namespace sf;
+
 
 int main() {
+    // The game will always be in 1 of 4 states
+    enum class State {PAUSED, LEVELING_UP, GAME_OVER, PLAYING};
+
+    // Start with the GAME_OVER state
+    State state = State::GAME_OVER;
+
+    // Get the screen resolution and create a window
+    Vector2f resolution;
+    resolution.x = VideoMode::getDesktopMode().width;
+    resolution.y = VideoMode::getDesktopMode().height;
+
+    RenderWindow window(VideoMode(resolution.x, resolution.y), "Zombie Shooter", Style::Default);
+
+    // Create a View for the main action
+    View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+    // The clock for timing everything
+    Clock clock;
+
+    // How long has the playing state been active?
+    Time gameTimeTotal;
+
+    // Where is the mouse in relation to the world coordinates?
+    Vector2f mouseWorldPosition;
+
+    // Where is the mouse in relation to the screen coordinates?
+    Vector2i mouseScreenPosition;
+
+    // Create an instance of the player class
+    Player player;
+
+    // The boundaries of the arena
+    IntRect arena;
+
+    // The main game loop
+    while (window.isOpen()) {
+        // Handle input //
+        
+        // Handle events by polling
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::KeyPressed) {
+                // Pause the game while playing
+                if (event.key.code == Keyboard::Return && state == State::PLAYING) {
+                    state = State::PAUSED;
+                }
+                // Restart while paused
+                else if (event.key.code == Keyboard::Return && state == State::PAUSED) {
+                    state = State::PLAYING;
+
+                    // Restart the clock so there isn't a frame jump
+                    clock.restart();
+                }
+                // Start a new game while in GAME_OVER state
+                else if ( event.key.code == Keyboard::Return && state == State::GAME_OVER) {
+                    state = State::LEVELING_UP;
+                }
+
+                if (state == State::PLAYING) {
+
+                }
+            }
+        } // End event polling
+
+        // Handle the player quitting
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+
+        // Handle WASD while playing
+        if (state == State::PLAYING) {
+            // Handle the pressing and releasing of WASD keys
+            if (Keyboard::isKeyPressed(Keyboard::W)) {
+                player.moveUp();
+            }
+            else {
+                player.stopUp();
+            }
+            
+            if (Keyboard::isKeyPressed(Keyboard::S)) {
+                player.moveDown();
+            }
+            else {
+                player.stopDown();
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                player.moveLeft();
+            }
+            else {
+                player.stopLeft();
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::D)) {
+                player.moveRight();
+            }
+            else {
+                player.stopRight();
+            }
+        } // End WASD while playing
+        
+    } // End game loop
+
     return 0;
 };
